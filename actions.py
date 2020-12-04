abaf772438722c1ca9ad8cbe243ec36baf0ba52e
sirinehaddouche <60438964+sirinehaddouche@users.noreply.github.com>
2020-12-04 15:01:14 +0100

    Update actions.py

--- actions.py
+++ actions.py
@@ -9,13 +9,13 @@ import requests
 class ActionSubscribe(Action):
 
      def name(self) -> Text:
-         return ""
+         return "action_subscribe"
 
      def run(self, dispatcher: CollectingDispatcher,
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          image = requests.get('http://shibe.online/api/shibes?count=1').json()[0]
 
-         dispatcher.utter_message("{}".format(image))
+         dispatcher.utter_message("Congratulatons! You have just subscribed to Rasa newsletter. As a little celebration gift, check out this cool doggo: {}".format(image))
 
          return []
